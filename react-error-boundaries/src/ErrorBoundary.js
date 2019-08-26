a2ec28f36f205e0e347aaa7ac3dabef28bfa2921
VijayRanghar <vijayranghar@gmail.com>
2019-08-26 14:35:03 +0530

    fix typo

--- react-error-boundaries/src/ErrorBoundary.js
+++ react-error-boundaries/src/ErrorBoundary.js
@@ -4,13 +4,18 @@ class ErrorBoundary extends Component {
   state = {
     hasError: false,
   }
+  componentDidCatch(error, info) {
+    if(error) {
+      this.setState({ error: true })
+    }
+  }
   render() {
     return (
       <div>
-        { hasError 
+        { this.state.hasError 
           ? 
           <h1 style={{color: 'red'}}> Sorry!! some error occured.</h1>
-          : this.props.childern
+          : this.props.children
         } 
       </div>
     );
